7f5cf91f365ff8b7d1a042e1f072683943abf7f2
faysaldon121-beep <faysaldon121@gmail.com>
2026-03-29 11:01:10 +0500

    Update route.ts

--- app/api/games/download/[slug]/file/route.ts
+++ app/api/games/download/[slug]/file/route.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import connectDB from '@/lib/mongodb';
 import Game from '@/models/Game';
 import { getValidatedResponse } from '@/lib/downloadSession';
-import { extractDownloadLink } from '@/lib/puppeteer-extractor.ts';
+import { extractDownloadLink } from '@/lib/puppeteer-extractor';
 
 export const runtime = 'nodejs';
 
