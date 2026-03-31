b75badd6d4f06a564a94e00ed27ba42e74ffe6db
faysaldon121-beep <faysaldon121@gmail.com>
2026-03-31 14:51:48 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import 'prismjs/themes/prism-tomorrow.css';
 import Header from "@/components/layout/Header";
 import Footer from "@/components/layout/Footer";
 import keywordsData from "@/lib/keywords.json";
