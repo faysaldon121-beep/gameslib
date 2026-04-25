f6e821d564973b73a7fd49463f8b3dcc18f2e3ee
faysaldon121-beep <faysaldon121@gmail.com>
2026-04-25 20:04:43 +0500

    Add NextStudio component to StudioPage

--- app/studio/[[...tool]]/page.tsx
+++ app/studio/[[...tool]]/page.tsx
@@ -1,1 +1,6 @@
-impor
+import { NextStudio } from 'next-sanity/studio'
+import config from '../../../../sanity.config'
+
+export default function StudioPage() {
+  return <NextStudio config={config} />
+}
