46d3756ffa61b259c5cc8174e2fce8e60bf2643f
faysaldon121-beep <faysaldon121@gmail.com>
2026-04-25 18:39:03 +0500

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,2 +1,23 @@
-import { defineConfig } from 'sanity'; import { deskTool } from 'sanity/desk'; import { visionTool } from '@sanity/vision'; import { codeInput } from '@sanity/code-input'; import { schemas } from './sanity/schemas';
-export default defineConfig({ name: 'default', title: 'GamesLib CMS', projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!, basePath: '/studio', plugins:[ deskTool(), visionTool(), codeInput() ], schema: { types: schemas } });
+'use client'
+
+import { defineConfig } from 'sanity'
+import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
+import { codeInput } from '@sanity/code-input'
+import { schemas } from './sanity/schemas'
+
+export default defineConfig({
+  name: 'default',
+  title: 'GamesLib CMS',
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  basePath: '/studio',
+  plugins: [
+    structureTool(),
+    visionTool(),
+    codeInput()
+  ],
+  schema: {
+    types: schemas
+  }
+})
