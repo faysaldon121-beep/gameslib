1baab5471d23e1163f72abf2861b59f45b95135e
faysaldon121-beep <faysaldon121@gmail.com>
2026-04-29 18:45:11 +0500

    Update imported icons in news page

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -11,7 +11,8 @@ import NewsletterSignup from '@/components/news/NewsletterSignup';
 import Pagination from '@/components/ui/Pagination';
 import { ClockIcon, FireIcon, NewspaperIcon, EyeIcon, ShareIcon } from '@heroicons/react/24/outline';
 import { NewsBase } from '@/types/news';
-import { FaEye, FaCalendarAlt, FaUser, FaTag } from 'react-icons/fa';
+//import { FaEye, FaCalendarAlt, FaUser, FaTag } from 'react-icons/fa';
+import { FaEye, FaShareAlt, FaCalendarAlt, FaUser, FaTag } from 'react-icons/fa';
 
 export const dynamic = 'force-dynamic';
 export const revalidate = 60;
